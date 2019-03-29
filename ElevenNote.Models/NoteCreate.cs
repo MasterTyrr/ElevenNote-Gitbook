5e3d6dab885f3ac899244c686f7510d0bc8316f0
MasterTyrr <SamRichards@users.noreply.github.com>
2019-03-29 15:07:21 -0400

    6.0  complete

--- ElevenNote.Models/NoteCreate.cs
+++ ElevenNote.Models/NoteCreate.cs
@@ -17,9 +17,7 @@ namespace ElevenNote.Models
         [MaxLength(8000)]
         public string Content { get; set; }
 
-        public override string ToString()
-        {
-            return base.ToString() => Title;
-        }
+        public override string ToString() => Title;
+
     }
 }
--- ElevenNote.WebMvc/Controllers/NoteController.cs
+++ ElevenNote.WebMvc/Controllers/NoteController.cs
@@ -22,5 +22,16 @@ namespace ElevenNote.WebMvc.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
     }
 }
